efb14ce6e12c0b62b665a5a1934512ac8d02069d
Darren Sarra <adriansarra18@gmail.com>
2020-04-19 19:57:44 -0400

    Created 'employee' table with 'id', 'first_name', 'last_name', 'role_id' and 'manager_id' as keys and id being the primary key.

--- db/schema.sql
+++ db/schema.sql
@@ -24,3 +24,21 @@ CREATE TABLE role (
         ON DELETE CASCADE,
     PRIMARY KEY(id)
 );
+
+-- Creates the table "employee" within employee_tracker_db --
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+    INDEX role_ind (role_id),
+    INDEX manager_ind (manager_id),
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
