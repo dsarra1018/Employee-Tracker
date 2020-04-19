55ccd9e6f173ce2cbfe788aab36604b943acc306
Darren Sarra <adriansarra18@gmail.com>
2020-04-19 17:03:21 -0400

    Created table 'department' with 'id' and 'name' as keys and 'id' as primary key.

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,11 @@ DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 -- Makes it so all of the following code will affect employee_tracker_db --
 USE employee_tracker_db;
+
+-- Creates the table "department" within employee_tracker_db --
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
