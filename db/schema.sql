f4bfd7eaf134e3a807560b6cd3d3fbd569fa8ce5
Darren Sarra <adriansarra18@gmail.com>
2020-04-19 16:58:31 -0400

    Created database.

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+-- Drops the employee_tracker_db if it exists currently --
+DROP DATABASE IF EXISTS employee_tracker_db;
+-- Create the "employee_tracker_db" --
+CREATE DATABASE employee_tracker_db;
+-- Makes it so all of the following code will affect employee_tracker_db --
+USE employee_tracker_db;
