447c7b572d697e31c19834bc8f8c5d8b1c632b68
Darren Sarra <adriansarra18@gmail.com>
2020-04-19 21:00:19 -0400

    Initialized employee_tracker.js with a welcome message.

---
+++ employee_tracker.js
@@ -0,0 +1,16 @@
+// Dependencies
+const inquirer = require('inquirer');
+const connection = require('./js/connection');
+
+// Initializes Content Management Systems
+function employeeTracker() {
+    
+    // Welcome message
+    console.log(`     +===========================================+
+     |                                           |
+     |    Welcome to Employee Tracker CLI App    |
+     |                                           |
+     +===========================================+`)
+};
+
+employeeTracker();
