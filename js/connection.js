c8a0978e508da5a951c06ddb8c8383b6d57b7b00
Darren Sarra <adriansarra18@gmail.com>
2020-04-19 20:22:18 -0400

    Initiated MySQL connection.

---
+++ js/connection.js
@@ -0,0 +1,23 @@
+// Dependencies
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+// MySQL DB Connection Information
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_tracker_db"
+});
+
+// Initiate MySQL Connection
+connection.connect((err) => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
